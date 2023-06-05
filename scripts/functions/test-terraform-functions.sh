e6c5147f0146ad904606388715528721bd0a6bd0
Tim Rickwood <tim.rickwood@nhs.net>
2023-06-05 17:26:45 +0100

    DR-85 Prototype terraform tests

--- scripts/functions/test-terraform-functions.sh
+++ scripts/functions/test-terraform-functions.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-source ./git-functions.sh
-source ./terraform-functions.sh
-
-terraform_change=$(git-check-if-commit-changed-directory $1 $2 $3)
-if [[  $terraform_change = true ]] ; then
-    terraform-format-check ../build
+export BRANCH_NAME=task/DPTS-2211_My_valid_branch
+export PRECOMMIT=false
+/bin/bash ./scripts/githooks/terraform-pre-commit.sh
+if [[ $? = 1 ]]; then
+    all_pass=1
 fi
 
 
