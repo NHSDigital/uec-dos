e8d511a74897075eb15bf2dc547224473e51627b
Tim Rickwood <tim.rickwood@nhs.net>
2023-06-06 12:08:39 +0100

    DR-85 Refactor terraform check

--- scripts/functions/test-terraform-functions.sh
+++ scripts/functions/test-terraform-functions.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-export BRANCH_NAME=task/DPTS-2211_My_valid_branch
-export PRECOMMIT=false
+all_pass=0
+export IAC_DIR="infrastructure"
 /bin/bash ./scripts/githooks/terraform-pre-commit.sh
-if [[ $? = 1 ]]; then
-    all_pass=1
-fi
-
 
--- scripts/githooks/terraform-pre-commit.sh
+++ scripts/githooks/terraform-pre-commit.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 source ./scripts/functions/git-functions.sh
-source ./scripts/functions/terraform-functions.sh
-set -e
 
-
-IAC_DIR=infrastructure
+TF_VERSION=1.4.6
 PRECOMMIT=${PRECOMMIT:-true}
+IAC_DIR=${IAC_DIR:-infrastructure}
 BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
 
-# check for terraform changes and formatting
-terraform_change=$(git-check-if-commit-changed-directory $PRECOMMIT "$BRANCH_NAME" $IAC_DIR)
-if [[ $terraform_change = true ]] ; then
-    terraform-format-check $IAC_DIR
-fi
+function main {
+  if [[ $(git-check-if-commit-changed-directory $PRECOMMIT $BRANCH_NAME $IAC_DIR) ]] ; then
+    cmd="fmt -recursive -check -list=true --diff $IAC_DIR"
+    docker run \
+      --volume=$PWD:/scan \
+      --workdir=/scan \
+		  hashicorp/terraform:$TF_VERSION $cmd
+  fi
+
+}
+
+main $*
