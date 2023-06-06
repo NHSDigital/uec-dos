2d588f5955bb70d0ddf3c93ec231e3a5367c8488
Tim Rickwood <tim.rickwood@nhs.net>
2023-06-06 16:40:17 +0100

    DRinvalidcommitmessage

--- scripts/githooks/git-commit-msg.sh
+++ scripts/githooks/git-commit-msg.sh
@@ -3,18 +3,10 @@ source ./scripts/functions/git-functions.sh
 set -e
 
 exit_code=0
-#BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
 BUILD_COMMIT_MESSAGE=${BUILD_COMMIT_MESSAGE:-"$(cat $1)"}
 
-# check branch name and commit message
-# check_git_branch_name $BRANCH_NAME
-#[ $? != 0 ] && exit_code=1 ||:
-# echo $exit_code
 check_git_commit_message "$BUILD_COMMIT_MESSAGE"
 echo $?
 echo twr
 exit $?
-# [ $? != 0 ] && exit_code=1 ||:
-# exit 1
 
-# exit $exit_code
