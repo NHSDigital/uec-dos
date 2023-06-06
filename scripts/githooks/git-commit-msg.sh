a4f8593b543aead601c0eade2fcf0c4693c4d6e5
Tim Rickwood <tim.rickwood@nhs.net>
2023-06-06 15:37:30 +0100

    DR85NotValid

--- scripts/githooks/git-commit-msg.sh
+++ scripts/githooks/git-commit-msg.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 source ./scripts/functions/git-functions.sh
-set -e
+# set -e
 
-BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
+exit_code=0
+#BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
 BUILD_COMMIT_MESSAGE=${BUILD_COMMIT_MESSAGE:-"$(cat $1)"}
 
 # check branch name and commit message
-check_git_branch_name $BRANCH_NAME
+# check_git_branch_name $BRANCH_NAME
+#[ $? != 0 ] && exit_code=1 ||:
+# echo $exit_code
 check_git_commit_message "$BUILD_COMMIT_MESSAGE"
+[ $? != 0 ] && exit_code=1 ||:
+echo $exit_code
+exit $exit_code
