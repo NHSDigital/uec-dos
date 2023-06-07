65e08641635e708d4bb65b2adc932166dc689443
Jonathan Pearce <j.pearce1@nhs.net>
2023-06-07 16:23:08 +0100

    DRinvalidcommitmessage-4

--- scripts/githooks/git-commit-msg.sh
+++ scripts/githooks/git-commit-msg.sh
@@ -2,11 +2,10 @@
 source ./scripts/functions/git-functions.sh
 # set -e
 
-exit_code=0
 BUILD_COMMIT_MESSAGE=${BUILD_COMMIT_MESSAGE:-"$(cat $1)"}
 
 check_git_commit_message "$BUILD_COMMIT_MESSAGE"
 echo $?
 echo twr
-exit $?
+exit 1
 
