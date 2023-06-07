26816204b69e7c6ba276eeac3e4e92602c4b10e3
Jonathan Pearce <j.pearce1@nhs.net>
2023-06-07 16:58:59 +0100

    DR-85 Fix to commit-msg hook

--- scripts/githooks/git-commit-msg.sh
+++ scripts/githooks/git-commit-msg.sh
@@ -4,6 +4,7 @@ source ./scripts/functions/git-functions.sh
 exit_code=0
 BUILD_COMMIT_MESSAGE=${BUILD_COMMIT_MESSAGE:-"$(cat $1)"}
 
-exit_code=check_git_commit_message "$BUILD_COMMIT_MESSAGE"
+check_git_commit_message "$BUILD_COMMIT_MESSAGE"
+exit_code=$?
 
 exit $exit_code
